f748496310fcdcd0a5ddd7b86c28a82bb3c24920
Jonathan Novaretti <jonn.novaretti@gmail.com>
2026-01-02 11:57:36 -0300

    Fix seeder entity resolution

--- apps/server/src/database/data-source.ts
+++ apps/server/src/database/data-source.ts
@@ -1,13 +1,11 @@
 import 'dotenv/config';
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { SeederOptions } from 'typeorm-extension';
-import { Template } from '../sites/entities/template.entity';
-import { Thumbnail } from '../sites/entities/thumbnail.entity';
 
 const options: DataSourceOptions & SeederOptions = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  entities: [Template, Thumbnail],
+  entities: ['src/sites/entities/**/*{.ts,.js}'],
 
   seeds: ['src/database/seeds/**/*{.ts,.js}'],
 };
--- apps/server/src/database/run-seeder.ts
+++ apps/server/src/database/run-seeder.ts
@@ -1,20 +1,18 @@
 import 'dotenv/config';
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { runSeeders } from 'typeorm-extension';
-import { Template } from '../sites/entities/template.entity';
-import { Thumbnail } from '../sites/entities/thumbnail.entity';
 
 (async () => {
   const options: DataSourceOptions = {
     type: 'postgres',
     url: process.env.DATABASE_URL,
-    entities: [Template, Thumbnail],
+    entities: ['src/sites/entities/**/*{.ts,.js}'],
   };
 
   const dataSource = new DataSource(options);
   await dataSource.initialize();
 
-  runSeeders(dataSource, {
+  await runSeeders(dataSource, {
     seeds: ['src/database/seeds/**/*{.ts,.js}'],
   });
 })();
