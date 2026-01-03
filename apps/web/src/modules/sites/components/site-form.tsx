68bae99e58f02530128ac8ccf721bee4423b7824
Jonathan Novaretti <jonn.novaretti@gmail.com>
2026-01-03 09:46:55 -0300

    adding steps

--- apps/web/src/modules/sites/components/site-form.tsx
+++ apps/web/src/modules/sites/components/site-form.tsx
@@ -81,140 +81,224 @@ export function SiteForm() {
   });
 
   return (
-    <Card className="mt-10 max-w-2xl mx-auto">
-      <div className="p-6">
-        <h2 className="text-2xl font-bold mb-6">Site</h2>
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(values => createSite.mutate(values))}
-            className="space-y-6"
-          >
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <div className="flex flex-col gap-2">
-                    <FormLabel>Name</FormLabel>
-                    <FormLabel className="text-xs font-extralight">
-                      Enter your company name
-                    </FormLabel>
-                  </div>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="description"
-              render={({ field }) => (
-                <FormItem>
-                  <div className="flex flex-col gap-2">
-                    <FormLabel>Description</FormLabel>
-                    <FormLabel className="text-xs font-extralight">
-                      Explain your business. What do you do? When was your
-                      company founded? What is your mission? Elaborate a good
-                      description, AI will use it to create your site.
-                    </FormLabel>
-                  </div>
-                  <FormControl>
-                    <Textarea {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="industry"
-              render={({ field }) => (
-                <FormItem>
-                  <div className="flex flex-col gap-2">
-                    <FormLabel>Industry</FormLabel>
-                    <FormLabel className="text-xs font-extralight">
-                      Select your company&apos;s industry
-                    </FormLabel>
-                  </div>
-                  <FormControl>
-                    <Select
-                      onValueChange={field.onChange}
-                      defaultValue={field.value}
-                    >
-                      <SelectTrigger>
-                        <SelectValue />
-                      </SelectTrigger>
-                      <SelectContent>
-                        {getIndustries().map(industry => (
-                          <SelectItem key={industry.code} value={industry.code}>
-                            <div className="flex items-center gap-2">
-                              {industry.name}
-                            </div>
-                          </SelectItem>
-                        ))}
-                      </SelectContent>
-                    </Select>
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="sections"
-              render={({ field }) => (
-                <FormItem>
-                  <div className="flex flex-col gap-2">
-                    <FormLabel>Sections</FormLabel>
-                    <FormLabel className="text-xs font-extralight">
-                      Select which sections you want to have in your site
-                    </FormLabel>
-                  </div>
-                  <FormControl>
-                    <CheckboxGroup className="gap-2">
-                      {getSections().map(section => (
-                        <div
-                          key={section.code}
-                          className="flex items-center gap-2"
-                        >
-                          <CheckboxGroupItem
-                            disabled={section.disabled}
-                            checked={field.value?.includes(section.code)}
-                            onChange={event => {
-                              const currentValues = field.value ?? [];
-                              const nextValues = event.target.checked
-                                ? Array.from(
-                                    new Set([...currentValues, section.code]),
-                                  )
-                                : currentValues.filter(
-                                    value => value !== section.code,
-                                  );
-                              field.onChange(nextValues);
-                            }}
-                          />
-                          <span className="text-sm font-normal">
-                            {section.name}
-                          </span>
-                        </div>
-                      ))}
-                    </CheckboxGroup>
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button
-              type="submit"
-              className="w-full"
-              disabled={createSite.isPending}
-            >
-              {createSite.isPending ? 'Creating...' : 'Create site'}
-            </Button>
-          </form>
-        </Form>
+    <>
+      <div className="flex flex-row">
+        <ol className="items-center w-full space-y-4 sm:flex sm:space-x-8 sm:space-y-0 rtl:space-x-reverse">
+          <li className="flex items-center text-fg-brand space-x-3 rtl:space-x-reverse">
+            <span className="flex items-center justify-center w-10 h-10 bg-brand-softer rounded-full lg:h-12 lg:w-12 shrink-0">
+              <svg
+                className="w-5 h-5 text-fg-brand"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M5 11.917 9.724 16.5 19 7.5"
+                />
+              </svg>
+            </span>
+            <span>
+              <h3 className="font-medium leading-tight">User info</h3>
+              <p className="text-sm">Step details here</p>
+            </span>
+          </li>
+          <li className="flex items-center text-body space-x-3 rtl:space-x-reverse">
+            <span className="flex items-center justify-center w-10 h-10 bg-neutral-tertiary rounded-full lg:h-12 lg:w-12 shrink-0">
+              <svg
+                className="w-5 h-5 text-body"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M15 9h3m-3 3h3m-3 3h3m-6 1c-.306-.613-.933-1-1.618-1H7.618c-.685 0-1.312.387-1.618 1M4 5h16a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1Zm7 5a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"
+                />
+              </svg>
+            </span>
+            <span>
+              <h3 className="font-medium leading-tight">Company info</h3>
+              <p className="text-sm">Step details here</p>
+            </span>
+          </li>
+          <li className="flex items-center text-body space-x-3 rtl:space-x-reverse">
+            <span className="flex items-center justify-center w-10 h-10 bg-neutral-tertiary rounded-full lg:h-12 lg:w-12 shrink-0">
+              <svg
+                className="w-5 h-5 text-body"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M3 10h18M6 14h2m3 0h5M3 7v10a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1Z"
+                />
+              </svg>
+            </span>
+            <span>
+              <h3 className="font-medium leading-tight">Payment info</h3>
+              <p className="text-sm">Step details here</p>
+            </span>
+          </li>
+        </ol>
       </div>
-    </Card>
+      <Card className="mt-10 max-w-2xl mx-auto">
+        <div className="p-6">
+          <h2 className="text-2xl font-bold mb-6">Site</h2>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(values => createSite.mutate(values))}
+              className="space-y-6"
+            >
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <div className="flex flex-col gap-2">
+                      <FormLabel>Name</FormLabel>
+                      <FormLabel className="text-xs font-extralight">
+                        Enter your company name
+                      </FormLabel>
+                    </div>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem>
+                    <div className="flex flex-col gap-2">
+                      <FormLabel>Description</FormLabel>
+                      <FormLabel className="text-xs font-extralight">
+                        Explain your business. What do you do? When was your
+                        company founded? What is your mission? Elaborate a good
+                        description, AI will use it to create your site.
+                      </FormLabel>
+                    </div>
+                    <FormControl>
+                      <Textarea {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="industry"
+                render={({ field }) => (
+                  <FormItem>
+                    <div className="flex flex-col gap-2">
+                      <FormLabel>Industry</FormLabel>
+                      <FormLabel className="text-xs font-extralight">
+                        Select your company&apos;s industry
+                      </FormLabel>
+                    </div>
+                    <FormControl>
+                      <Select
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                      >
+                        <SelectTrigger>
+                          <SelectValue />
+                        </SelectTrigger>
+                        <SelectContent>
+                          {getIndustries().map(industry => (
+                            <SelectItem
+                              key={industry.code}
+                              value={industry.code}
+                            >
+                              <div className="flex items-center gap-2">
+                                {industry.name}
+                              </div>
+                            </SelectItem>
+                          ))}
+                        </SelectContent>
+                      </Select>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="sections"
+                render={({ field }) => (
+                  <FormItem>
+                    <div className="flex flex-col gap-2">
+                      <FormLabel>Sections</FormLabel>
+                      <FormLabel className="text-xs font-extralight">
+                        Select which sections you want to have in your site
+                      </FormLabel>
+                    </div>
+                    <FormControl>
+                      <CheckboxGroup className="gap-2">
+                        {getSections().map(section => (
+                          <div
+                            key={section.code}
+                            className="flex items-center gap-2"
+                          >
+                            <CheckboxGroupItem
+                              disabled={section.disabled}
+                              checked={field.value?.includes(section.code)}
+                              onChange={event => {
+                                const currentValues = field.value ?? [];
+                                const nextValues = event.target.checked
+                                  ? Array.from(
+                                      new Set([...currentValues, section.code]),
+                                    )
+                                  : currentValues.filter(
+                                      value => value !== section.code,
+                                    );
+                                field.onChange(nextValues);
+                              }}
+                            />
+                            <span className="text-sm font-normal">
+                              {section.name}
+                            </span>
+                          </div>
+                        ))}
+                      </CheckboxGroup>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button
+                type="submit"
+                className="w-full"
+                disabled={createSite.isPending}
+              >
+                {createSite.isPending ? 'Creating...' : 'Create site'}
+              </Button>
+            </form>
+          </Form>
+        </div>
+      </Card>
+    </>
   );
 }
